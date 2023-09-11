df1036b82ef5b1678ca5be3a45c145c4b8362c6c
MartinSchiemer <martin.schiemer94@gmail.com>
2023-09-11 19:03:10 +0100

    normal - made model initialisation seeded

--- trainer.py
+++ trainer.py
@@ -206,9 +206,9 @@ def _set_random(seed):
   # TODO want same model for each experiment start?
   random.seed(seed)
   np.random.seed(seed)
-  torch.manual_seed(1)
-  torch.cuda.manual_seed(1)
-  torch.cuda.manual_seed_all(1)
+  torch.manual_seed(seed)
+  torch.cuda.manual_seed(seed)
+  torch.cuda.manual_seed_all(seed)
   torch.backends.cudnn.deterministic = True
   torch.backends.cudnn.benchmark = False
 
