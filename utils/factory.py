2dfc9e6c9de38e5277503cd6c6604c5ec1be73e3
Clemens Schaefer <clemensschaefer1994@googlemail.com>
2024-07-15 17:36:13 -0700

    Update factory.py

--- utils/factory.py
+++ utils/factory.py
@@ -1,16 +1,8 @@
-from models.coil import COIL
 from models.der import DER
-# from models.ewc import EWC
-# from models.finetune import Finetune
 from models.foster import FOSTER
-# from models.gem import GEM
 from models.icarl import iCaRL
 from models.lwf import LwF
-# from models.replay import Replay
 from models.bic import BiC
-# from models.podnet import PODNet
-# from models.rmm import RMM_FOSTER, RMM_iCaRL
-# from models.wa import WA
 from models.memo import MEMO
 
 
@@ -22,30 +14,12 @@ def get_model(model_name, args):
     return BiC(args)
   elif name == "lwf":
     return LwF(args)
-  # elif name == "ewc":
-  #     return EWC(args)
-  # elif name == "wa":
-  #     return WA(args)
   elif name == "der":
       return DER(args)
-  # elif name == "finetune":
-  #     return Finetune(args)
-  # elif name == "replay":
-  #     return Replay(args)
-  # elif name == "gem":
-  #     return GEM(args)
-  elif name == "coil":
-      return COIL(args)
   elif name == "foster":
       return FOSTER(args)
-  # elif name == "rmm-icarl":
-  #     return RMM_iCaRL(args)
-  # elif name == "rmm-foster":
-  #     return RMM_FOSTER(args)
   elif name == 'memo':
       return MEMO(args)
-  # elif name == "podnet":
-  #     return PODNet(args)
   else:
     assert 0
 
